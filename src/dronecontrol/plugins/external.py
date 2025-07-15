e7006fa078f2f55dffa0244da2a9876df0e11502
Mattern <Dominik.Mattern@thi.de>
2025-07-15 17:33:01 +0200

    UDP plugin now works like a server, sending data in response to incoming requests

--- src/dronecontrol/plugins/external.py
+++ src/dronecontrol/plugins/external.py
@@ -1,25 +1,105 @@
 """ Plugins for communication to other software
+
+Currently only features a basic UDP server which sends data on connected drones and running missions in a json format.
 """
 import asyncio
 import socket
 import json
+import time
+import math
 
 from dronecontrol.plugin import Plugin
+from dronecontrol.utils import coroutine_awaiter
 
-# TODO: Different process, maybe wait for client to ask for info, then start sending?
+
+class UDPClient:
+
+    def __init__(self, ip, port, frequency, duration):
+        self.start_time = time.time()
+        self.ip = ip
+        self.port = port
+        self.frequency = frequency
+        self.duration = duration
 
 
 class UDPPlugin(Plugin):
+    """ Communication happens over port 31659. A client will send a json message with the desired frequency and duration
+    (in seconds) to this port and the server starts answering. A duration of 0 means infinite. Frequency is capped between 1/60 and 20Hz.
+
+    Example message from client:
+    {
+      "duration": 30,
+      "frequency": 5
+    }
+
+    """
 
     def __init__(self, dm, logger, name):
         super().__init__(dm, logger, name)
-        self.port = 31659
+        self.inport = 31659
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setblocking(False)
+        sock.bind(("", self.inport))
         self.socket = sock
+
+        outsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.outsocket = outsock
         self.frequency = 5
         self.background_functions = [
-            self._send_continuously()
+            #self._send_continuously(),
+            self._listen_for_clients(),
         ]
+
+    async def _listen_for_clients(self):
+        self.logger.debug("Listening for clients...")
+        while True:
+            try:
+                rec_task = asyncio.get_running_loop().sock_recvfrom(self.socket, 1024)
+                self._running_tasks.add(rec_task)
+                msg, addr = await rec_task
+                self.logger.debug(f"Received message from {addr}")
+                json_dict = json.loads(msg)
+                if "frequency" not in json_dict or "duration" not in json_dict:
+                    self.logger.debug(f"Invalid JSON message received from {addr}")
+                    continue
+                ip, port = addr
+                frequency = json_dict["frequency"]
+                if frequency > 20:
+                    frequency = 20
+                elif frequency < 1/60:
+                    frequency = 1/60
+                duration = json_dict["duration"]
+                if duration <= 0:
+                    duration = 0
+                client = UDPClient(ip, port, frequency, duration)
+                send_task = asyncio.create_task(self._client_sender(client))
+                awaiter_task = asyncio.create_task(coroutine_awaiter(send_task, self.logger))
+                self._running_tasks.add(send_task)
+                self._running_tasks.add(awaiter_task)
+                self.logger.info(f"New client @{ip, port} with frequency {frequency} and duration {math.inf if duration == 0 else duration}.")
+            except TimeoutError:
+                pass
+            except Exception as e:
+                self.logger.warning("Exception listening for incoming UDP!")
+                self.logger.debug(repr(e), exc_info=True)
+
+    async def _client_sender(self, client: UDPClient):
+        """ Send data to the client.
+        """
+        # TODO: If we ever have a larger number of clients we should cache the jsons somehow
+        # TODO: The OSError for the send command is raised at the recvfrom for some reason, which breaks this client handling
+        while client.duration == 0 or time.time() < (client.start_time + client.duration):
+            try:
+                data = self._make_json()
+                self._send_msg(data, client.ip, client.port)
+                await asyncio.sleep(1/client.frequency)
+            except OSError as e:
+                self.logger.info("Couldn't send information, closing connection...")
+                self.logger.info(f"{e.errno}: {e.strerror}")
+                break
+            except Exception as e:
+                self.logger.warning("Exception sending data out over UDP! Check the log for details.")
+                self.logger.debug(repr(e), exc_info=True)
 
     async def _send_continuously(self):
         while True:
@@ -64,9 +144,11 @@ class UDPPlugin(Plugin):
                         self.logger.debug(repr(e), exc_info=True)
         return json.dumps(data)
 
-    def _send_msg(self, msg: str):
+    def _send_msg(self, msg: str, ip="localhost", port=None):
+        if port is None:
+            port = self.inport
         try:
-            self.socket.sendto(msg.encode("utf-8"), ("localhost", self.port))
+            self.outsocket.sendto(msg.encode("utf-8"), (ip, port))
         except Exception as e:
             self.logger.warning("Exception sending out data! Check the log for details.")
             self.logger.debug(repr(e), exc_info=True)
---
+++ udp_dummyclient.py
@@ -0,0 +1,44 @@
+""" Plugins for communication to other software
+"""
+import asyncio
+import socket
+import json
+
+LISTEN_TIME = 10
+
+
+class UDPClient:
+
+    def __init__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("", 0))
+        self.socket = sock
+        self.target = ("localhost", 31659)
+        self.duration = 30
+        self.frequency = 1
+
+    async def _send_init_message(self):
+        msg = json.dumps({"duration": self.duration, "frequency": self.frequency})
+        self.socket.sendto(msg.encode("utf-8"), self.target)
+
+    async def _receive(self):
+        while True:
+            try:
+                msg = await asyncio.wait_for(asyncio.get_running_loop().sock_recv(self.socket, 1024), LISTEN_TIME)
+                json_str = json.loads(msg)
+                print(json_str)
+            except TimeoutError:
+                print("No messages...")
+            except Exception as e:
+                print("Exception receiving data out over UDP!", repr(e))
+
+
+async def main():
+    receiver = UDPClient()
+    await receiver._send_init_message()
+    await receiver._receive()
+    receiver.socket.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
