9e9bde5eef087fcf8da5bfe7937717d3f7ef3b0e
Domattee <domattee@yahoo.de>
2026-08-15 21:47:17 +0200

    More testing

--- docs/conf.py
+++ docs/conf.py
@@ -28,10 +28,10 @@ numpydoc_class_members_toctree = False
 automodapi_toctreedirnm = 'generated'
 automodsumm_inherited_members = True
 autodoc_preserve_defaults = True
-autoclass_content = 'both'
+autoclass_content = 'class'
 
 napoleon_google_docstring = True
-napoleon_use_ivar = True
+#napoleon_use_ivar = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
