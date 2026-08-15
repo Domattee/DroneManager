c2c093e1bbd859b367e63346d6495711e3b32206
Domattee <domattee@yahoo.de>
2026-08-16 01:45:16 +0200

    Fixed docs and linting for sensors package

---
+++ src/dronemanager/sensors/__init__.py
@@ -0,0 +1,1 @@
+"""Package containing different sensor plugin implementations."""
--- src/dronemanager/sensors/ecowitt.py
+++ src/dronemanager/sensors/ecowitt.py
@@ -1,10 +1,12 @@
-"""Sensor package for an ecowitt gw1100"""
+"""Sensor plugin for an ecowitt GW1100."""
 import asyncio
+import logging
 
 import requests
 import math
 import datetime
 
+import dronemanager.core
 from dronemanager.plugins.sensor import Sensor
 
 # Leitstand IP: 192.168.1.41
@@ -43,72 +45,114 @@ ECOWITT_ID_MAP_RAIN = {
 
 
 class WeatherDataEntry:
+    """Data class for a single type of weather information.
+
+    Contains components: The label for the information, the value and the unit, i.e. "temperature", 25.3, "C".
+    """
     def __init__(self, name: str, value: float = math.nan, unit: str = ""):
-        self.name = name
-        self.value = value
-        self.unit = unit
+        """Create a WeatherDataEntry object.
+
+        Args:
+            name: A label for the information.
+            value: The value of the metric.
+            unit: The unit of the metric.
+        """
+        self.name = name  #: A label for the information.
+        self.value = value  #: The value of the metric.
+        self.unit = unit  #: The unit of the metric.
 
 
 class WeatherData:
+    """Data class for weather information."""
 
-    def __init__(self, timestamp=None):
-        self.temperature = WeatherDataEntry("Temperature")
-        self.dew_point = WeatherDataEntry("Dew point")
-        self.humidity = WeatherDataEntry("Humidity")
-        self.light = WeatherDataEntry("Light")
-        self.uvi = WeatherDataEntry("UVI")
-        self.wind_speed = WeatherDataEntry("Wind Speed")
-        self.wind_direction = WeatherDataEntry("Wind Direction")
-        self.gust_speed = WeatherDataEntry("Gust Speed")
-        self.wind_speed_max_day = WeatherDataEntry("Day max wind")
+    def __init__(self, timestamp: datetime.datetime = None):
+        """Create WeatherData object.
 
-        self.rain_event = WeatherDataEntry("Rain Event")
-        self.rain_rate = WeatherDataEntry("Current Rain Rate")
-        self.cum_rain_today = WeatherDataEntry("Rain this Day")
-        self.cum_rain_week = WeatherDataEntry("Rain this Week")
-        self.cum_rain_month = WeatherDataEntry("Rain this Month")
-        self.cum_rain_year = WeatherDataEntry("Rain Year")
+        Args:
+            timestamp: A time stamp for the data.
+        """
+        self.temperature = WeatherDataEntry("Temperature")  #: Current temperature
+        self.dew_point = WeatherDataEntry("Dew point")  #: Current dew point
+        self.humidity = WeatherDataEntry("Humidity")  #: Current humidity
+        self.light = WeatherDataEntry("Light")  #: Current solar irradiance
+        self.uvi = WeatherDataEntry("UVI")  #: Current UV index
+        self.wind_speed = WeatherDataEntry("Wind Speed")  #: Current wind speed
+        self.wind_direction = WeatherDataEntry("Wind Direction")  #: Wind direction, 0 North
+        self.gust_speed = WeatherDataEntry("Gust Speed")  #: Gust speed
+        self.wind_speed_max_day = WeatherDataEntry("Day max wind")  #: Highest wind speed today
 
-        self.pressure = WeatherDataEntry("Pressure")
-        self.time = timestamp
+        self.rain_event = WeatherDataEntry("Rain Event")  #: Rain event
+        self.rain_rate = WeatherDataEntry("Current Rain Rate")  #: Current rain rate
+        self.cum_rain_today = WeatherDataEntry("Rain this Day")  # Cumulative rain this day
+        self.cum_rain_week = WeatherDataEntry("Rain this Week")  #: Cumulative rain this week
+        self.cum_rain_month = WeatherDataEntry("Rain this Month")  #: Cumulative rain this month
+        self.cum_rain_year = WeatherDataEntry("Rain Year")  #: Cumulative rain this year
+
+        self.pressure = WeatherDataEntry("Pressure")  #: Atmospheric pressure
+        self.time = timestamp  #: The time of the data collection
+        #: A list of the attribute, other than time, that get written out
         self.data_entries = [self.temperature, self.dew_point, self.humidity, self.light, self.uvi, self.wind_speed,
                              self.wind_direction, self.gust_speed, self.wind_speed_max_day, self.rain_event,
                              self.rain_rate, self.cum_rain_today, self.cum_rain_week, self.cum_rain_month,
                              self.cum_rain_year, self.pressure]
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return weather data as a nice string representation.
+
+        Returns:
+            A string with the weather data.
+        """
         return f"Time {self.time}\t" + "\t".join([f"{entry.name}: {entry.value}{entry.unit}"
                                                   for entry in self.data_entries])
 
     @classmethod
-    def from_dict(cls, input_dict, timestamp=None):
+    def from_response_json(cls, response_json: dict, timestamp: datetime.datetime | None = None) -> "WeatherData":
+        """Parse the response json into a WeatherData object.
+
+        Also translates the id bytes to readable strings.
+
+        Args:
+            response_json: The json to be parsed.
+            timestamp: The timestamp for the data.
+
+        Returns:
+            A WeaatherData object.
+        """
         output = cls(timestamp=timestamp)
         # Parse common list entry
-        if "common_list" in input_dict:
-            for entry in input_dict["common_list"]:
+        if "common_list" in response_json:
+            for entry in response_json["common_list"]:
                 entry_id, entry_value, entry_unit = output.parse_xml_entry(entry)
                 if entry_id in ECOWITT_ID_MAP_COMMON:
                     attr_name = ECOWITT_ID_MAP_COMMON[entry_id]
                     data_entry = output.__getattribute__(attr_name)
                     data_entry.value = entry_value
                     data_entry.unit = entry_unit
-        if "rain" in input_dict:
-            for entry in input_dict["rain"]:
+        if "rain" in response_json:
+            for entry in response_json["rain"]:
                 entry_id, entry_value, entry_unit = output.parse_xml_entry(entry)
                 if entry_id in ECOWITT_ID_MAP_RAIN:
                     attr_name = ECOWITT_ID_MAP_RAIN[entry_id]
                     data_entry = output.__getattribute__(attr_name)
                     data_entry.value = entry_value
                     data_entry.unit = entry_unit
-        if "wh25" in input_dict:
-            entry = input_dict["wh25"][0]
+        if "wh25" in response_json:
+            entry = response_json["wh25"][0]
             valunit = entry["abs"]
             value, unit = valunit.split(" ")
             output.pressure.value = value
             output.pressure.unit = unit
         return output
 
-    def parse_xml_entry(self, entry):
+    def parse_xml_entry(self, entry: dict) -> tuple[str, float, str]:
+        """Parse a single entry from the HTML response.
+
+        Args:
+            entry: The entry to be parsed.
+
+        Returns:
+            The parsed id, value and unit from the entry.
+        """
         entry_id = entry.get("id")
         entry_value = entry.get("val", math.nan)
         entry_unit = entry.get("unit", None)
@@ -126,8 +170,14 @@ class WeatherData:
         entry_value = float(entry_value)
         return entry_id, entry_value, entry_unit
 
-    def to_json_dict(self):
-        """Create a json serializable dictionary"""
+    def to_json_dict(self) -> dict:
+        """Create a json serializable dictionary.
+
+        The output can be used to write the data to file, for example with json.dump.
+
+        Returns:
+            The data as a serializable dictionary.
+        """
         out_dict = self.__dict__.copy()
         out_dict["time"] = self.time.isoformat()
         out_dict.pop("data_entries")
@@ -137,8 +187,15 @@ class WeatherData:
         return out_dict
 
     @classmethod
-    def from_json_dict(cls, json_dict):
-        """Recreate the object from a json serialized dictionary"""
+    def from_json_dict(cls, json_dict: dict) -> "WeatherData":
+        """Recreate the object from a dictionary, such as from json.loads.
+
+        Args:
+            json_dict: The json dictionary.
+
+        Returns:
+            The data as a WeatherData object.
+        """
         timestamp = datetime.datetime.fromisoformat(json_dict.pop("time"))
         new_obj = cls(timestamp)
         for attr_name, attr_value in json_dict.items():
@@ -152,14 +209,30 @@ class WeatherData:
 class EcoWittSensor(Sensor):
     """Class for EcoWitt Weather stations that support their HTTP API."""
 
-    def __init__(self, dm, logger, name="ecowitt"):
-        super().__init__(dm, logger, name)
-        self.ip = None
-        self.last_data: WeatherData | None = None
-        self.time_since_ping = math.nan
+    def __init__(self, dm: dronemanager.core.DroneManager, logger: logging.Logger, name: str = "ecowitt"):
+        """Create an EcoWittSensor.
 
-    async def connect(self, ip: str):
-        """No connection procedure"""
+        Args:
+            dm: The associated DroneManager instance.
+            logger: The logger to use for any output or errors.
+            name: The name to use for this plugin. Default "ecowitt".
+        """
+        super().__init__(dm, logger, name)
+        self.ip = None  #: The ip of the sensor.
+        self.last_data: WeatherData | None = None  #: The last data we received.
+
+    async def connect(self, ip: str) -> bool:
+        """Connect to the EcoWitt sensor.
+
+        No connection procedure as such. Instead, we try to request the data and return false if there is no response
+        or an exception.
+
+        Args:
+            ip: The IP address of the sensor.
+
+        Returns:
+            Whether we got a response with HTTP status code 200.
+        """
         self.logger.info(f"Adding sensor with {ip}...")
         try:
             response = await asyncio.get_running_loop().run_in_executor(None, requests.get,
@@ -177,12 +250,22 @@ class EcoWittSensor(Sensor):
             return False
 
     async def get_data(self) -> WeatherData | None:
+        """Request data from the sensor.
+
+        Gets an HTTP response from the sensor and then parses that into :py:class:`WeatherData`.
+        The timestamp is applied by this function, not the sensor itself.
+
+        Returns None if there was an error.
+
+        Returns:
+            Either the data or None if there was an error.
+        """
         try:
             timestamp = datetime.datetime.now(datetime.UTC)
             response = await asyncio.get_running_loop().run_in_executor(None,
                                                                         requests.get,
                                                                         f"http://{self.ip}/get_livedata_info")
-            data = WeatherData.from_dict(response.json(), timestamp=timestamp)
+            data = WeatherData.from_response_json(response.json(), timestamp=timestamp)
             self.last_data = data
             return data
         except Exception as e:
@@ -191,9 +274,9 @@ class EcoWittSensor(Sensor):
             return None
 
     async def status(self):
-        """No status as such to report."""
+        """No status as such to report, instead print core information."""
         self.logger.info(f"EcoWitt sensor as {self.PREFIX} with IP {self.ip}")
 
     async def disconnect(self):
-        """No disconnect procedure."""
+        """Dummy function since there is no disconnect procedure."""
         pass
