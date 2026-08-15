92741842eabdfdda06a3459cd635d64732e4a482
Domattee <domattee@yahoo.de>
2026-08-15 21:58:18 +0200

    More experiments

--- docs/conf.py
+++ docs/conf.py
@@ -49,3 +49,52 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 autosummary_generate = False
+
+
+from typing import ClassVar, get_origin
+
+
+_current_class = None
+_instance_attributes = {}
+
+
+def _is_classvar(annotation):
+    """Return whether an annotation is a ClassVar."""
+    return get_origin(annotation) is ClassVar
+
+
+def _process_class_docstring(app, what, name, obj, options, lines):
+    """Record instance attributes declared by each class."""
+    global _current_class
+
+    if what != "class":
+        return
+
+    _current_class = obj
+
+    annotations = getattr(obj, "__annotations__", {})
+
+    _instance_attributes[id(obj)] = {
+        attribute_name
+        for attribute_name, annotation in annotations.items()
+        if not _is_classvar(annotation)
+    }
+
+
+def _skip_instance_attribute(app, what, name, obj, skip, options):
+    """Skip annotated instance attributes from autodoc."""
+    if what != "attribute":
+        return skip
+
+    if _current_class is None:
+        return skip
+
+    if name in _instance_attributes.get(id(_current_class), set()):
+        return True
+
+    return skip
+
+
+def setup(app):
+    app.connect("autodoc-process-docstring", _process_class_docstring)
+    app.connect("autodoc-skip-member", _skip_instance_attribute)
